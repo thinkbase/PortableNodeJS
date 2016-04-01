579eea641aade13c60c9c160e14bec34062a65fe
thinkbase <thinkbase@users.noreply.github.com>
2016-04-01 11:49:14 +0800

    Windows shell - change prompt

--- node-cmd.bat
+++ node-cmd.bat
@@ -7,6 +7,6 @@ set BASE_DIR=%cd%
 popd
 
 SET PATH=.\node_modules\.bin;%BASE_DIR%\scripts;%BASE_DIR%\scripts\inst\bin\npm-global\bin;%PATH%
-call %ComSpec% /K "PROMPT=[NodeJS]$g$s"
+call %ComSpec% /K "PROMPT=[NodeJS] $p$g$s"
 
 ENDLOCAL
